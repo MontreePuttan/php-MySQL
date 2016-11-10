6ccaa23d4b621fc69f14e3afd6dd6285cf1bef65
Montree221133 <scoopy_825@hotmail.com>
2016-11-10 21:29:31 +0700

    Start Project

---
+++ show_data.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+<table border="1">
+    <thead>
+    <tr>
+        <th>ID</th>
+        <th>Name</th>
+        <th>Lastname</th>
+        <th>Edit</th>
+
+    </tr>
+    </thead>
+    <tbody>
+    <?php
+    include "db_connect.php";
+    $sql_member = "select * from member";
+    $objQuery_member = mysql_query($sql_member) or die(mysql_error());
+    while ($result_member = mysql_fetch_array($objQuery_member)){
+        ?>
+        <tr>
+            <td><?php echo $result_member['id']; ?></td>
+            <td><?php echo $result_member['name']; ?></td>
+            <td><?php echo $result_member['lastname']; ?></td>
+            <td><a href="update_date.php?id=<?php echo $result_member['id']; ?>">edit</a> </td>
+        </tr>
+    <?php } ?>
+    </tbody>
+</table>
+</body>
+</html>
---
+++ update_data.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: NaT
+ * Date: 10/11/2559
+ * Time: 21:27
+ */
